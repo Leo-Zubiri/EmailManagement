092bf5e425582a188d2d4bd3302cd0131b6fcde5
Leo-Zubiri <leo.zubiri@outlook.com>
2024-03-14 23:00:10 -0600

    Add [EmailClient] new class to manage the smtp client

---
+++ EmailClient.cs
@@ -0,0 +1,43 @@
+﻿
+namespace EmailManagement
+{
+    public class EmailClient
+    {
+        public string smtpClient { get; set; }
+        public int port { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+        public bool enableSSL { get; set; } = true;
+
+        /// <summary>
+        /// enableSSL default value its true
+        /// </summary>
+        /// <param name="_smtpClient"></param>
+        /// <param name="_port"></param>
+        /// <param name="_user"></param>
+        /// <param name="_password"></param>
+        public EmailClient(string _smtpClient,int _port,string _user, string _password)
+        {
+            smtpClient = _smtpClient;
+            port = _port;
+            user = _user;
+            password = _password;
+        }
+
+        /// <summary>
+        /// You can set the enableSSL value to true/false
+        /// </summary>
+        /// <param name="_smtpClient"></param>
+        /// <param name="_port"></param>
+        /// <param name="_user"></param>
+        /// <param name="_password"></param>
+        public EmailClient(string _smtpClient, int _port, string _user, string _password, bool _enableSSL)
+        {
+            smtpClient = _smtpClient;
+            port = _port;
+            user = _user;
+            password = _password;
+            enableSSL = _enableSSL;
+        }
+    }
+}
